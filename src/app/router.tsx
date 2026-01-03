4989bd304d9cd1631b80ddbb61c5528f9eadc771
Meeeseeksss <armenkay03@gmail.com>
2026-01-03 21:15:34 +0200

    STT

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,37 +1,43 @@
 import { createBrowserRouter } from "react-router-dom";
 import { AppShell } from "../layout/AppShell";
+import { ScrollToTop } from "./ScrollToTop";
+
+import { HomePage } from "../pages/HomePage";
 import { MarketsPage } from "../pages/MarketsPage";
 import { MarketDetailPage } from "../pages/MarketDetailPage";
-import { CategoryPage } from "../pages/CategoryPage";
-import { CollectionsPage } from "../pages/CollectionsPage";
-import { CollectionDetailPage } from "../pages/CollectionDetailPage";
 import { PortfolioPage } from "../pages/PortfolioPage";
-import { WalletPage } from "../pages/WalletPage";
 import { ActivityPage } from "../pages/ActivityPage";
 import { WatchlistPage } from "../pages/WatchlistPage";
+import { WalletPage } from "../pages/WalletPage";
+import { ProfilePage } from "../pages/ProfilePage";
 import { LoginPage } from "../pages/LoginPage";
 import { SignupPage } from "../pages/SignupPage";
-import { ProfilePage } from "../pages/ProfilePage";
-import { HomePage } from "../pages/HomePage";
+import { CollectionsPage } from "../pages/CollectionsPage";
+import { CollectionDetailPage } from "../pages/CollectionDetailPage";
+import { CategoryPage } from "../pages/CategoryPage";
 
 export const router = createBrowserRouter([
-  { path: "/login", element: <LoginPage /> },
-  { path: "/signup", element: <SignupPage /> },
-
   {
-    element: <AppShell />,
+    element: (
+      <>
+        <ScrollToTop />
+        <AppShell />
+      </>
+    ),
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/markets", element: <MarketsPage /> },
-      { path: "/markets/:id", element: <MarketDetailPage /> },
-      { path: "/categories/:category", element: <CategoryPage /> },
+      { path: "/markets/:marketId", element: <MarketDetailPage /> },
+      { path: "/portfolio", element: <PortfolioPage /> },
+      { path: "/activity", element: <ActivityPage /> },
+      { path: "/watchlist", element: <WatchlistPage /> },
+      { path: "/wallet", element: <WalletPage /> },
+      { path: "/profile", element: <ProfilePage /> },
+      { path: "/login", element: <LoginPage /> },
+      { path: "/signup", element: <SignupPage /> },
       { path: "/collections", element: <CollectionsPage /> },
       { path: "/collections/:id", element: <CollectionDetailPage /> },
-      { path: "/watchlist", element: <WatchlistPage /> },
-      { path: "/portfolio", element: <PortfolioPage /> },
-      { path: "/wallet", element: <WalletPage /> },
-      { path: "/activity", element: <ActivityPage /> },
-      { path: "/profile", element: <ProfilePage /> },
+      { path: "/category/:slug", element: <CategoryPage /> },
     ],
   },
 ]);
